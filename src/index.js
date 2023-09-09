6df351e1bf6743c3cddf521905f8228ee5074094
WalterBarriosS <w.barrios.silva@gmail.com>
2023-09-09 14:23:48 -0500

    Ejercicios 1.1-1.2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+var total = 0;
+const Header = (props) =>{
+  return(
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Content = (props)=>{
+  total += parseFloat(props.exercises)
+  return(
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+const Total = ()=>{
+  return(
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
+const App = () => {
+  return (
+    <>
+      <Header course="Half Stack application development"/>
+      <Content part="Fundamentals of React" exercises="10" />
+      <Content part="Using props to pass data" exercises="7" />
+      <Content part="State of a component" exercises="14" />
+      <Total/>
+    </>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(<App />, document.getElementById('root')) 
